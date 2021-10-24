26fcc7329203280335849afbe4ab2762e6294213
Manuel Cerdas <manuel.cerdas.salazar@outlook.com>
2021-10-23 18:12:23 -0600

    Initial commit

---
+++ MailMerge/MailMerge/Controllers/TemplateController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailMerge.Controllers
+{
+    public class TemplateController : ControllerBase
+    {
+        private readonly ILogger<TemplateController> _logger;
+
+        public TemplateController(ILogger<TemplateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("list")]
+        [Produces("application/json")]
+        public IEnumerable<string> ListTemplates()
+        {
+            string[] aux = Directory.GetFiles("Templates", "*.*", SearchOption.AllDirectories);
+
+            List<string> result = new List<string>(aux);
+            return result;
+        }
+    }
+}
